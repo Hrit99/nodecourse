ef7092eeb4bf9cdcd871f6abb248f8958dbf596e
Hrit99 <hritikpihal51299@gmail.com>
2021-10-18 12:56:03 +0530

    changed port number

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
-
+// const logger = require('./middleware/logger')
+const morgan = require('morgan');
 //load env vars
 
 dotenv.config({path: './config/config.env' });
 
 const app = express();
 
+//dev login middlware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 app.listen((PORT), () => {
     console.log(`Server running in  ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
